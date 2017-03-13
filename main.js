20620ceaaf669a9b333e9009db7e830bc14e9ec8
dav2113534 <dpalacios03081991@gmail.com>
2017-03-13 08:11:13 -0700

    started deleteItem function

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //strictly javascript 
-//state is an object where are shopping list items are going to be stored 
+//state is an object where our shopping list items are going to be stored 
 var state = {
     items: []
 };
+
+// Logic behind the shopping list and what it should do 
+//When we create a shopping list we add items, once we have selected an item we check the box
+// after we check it we then delete the item from the list 
 
 //adds items into our state 
 function addItem(state, item) {
@@ -10,4 +14,10 @@ function addItem(state, item) {
         name: item,
         checked: false,
     })
+}
+
+//Next a function that deletes items once checked 
+
+function deleteItem() {
+    state.splice([items])
 }
